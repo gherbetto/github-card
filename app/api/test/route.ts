5722bf5bc48452a4351a0fbda30579ed4d040aa9
Dmitrii Gherastovschi <dgherastovschi@gmail.com>
2026-05-11 15:38:00 +0500

    feat: reaching to github api

---
+++ app/api/test/route.ts
@@ -0,0 +1,23 @@
+// export async function GET() {
+//     return Response.json({
+//         hasToken: !!process.env.GITHUB_TOKEN,
+//         username: process.env.GITHUB_USERNAME,
+//     })
+// }
+
+export async function GET() {
+  const res = await fetch("https://api.github.com/users/gherbetto", {
+    headers: {
+      Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+    },
+  });
+
+  const data = await res.json();
+
+  return Response.json({
+    login: data.login,
+    followers: data.followers,
+    public_repos: data.public_repos,
+    status: res.status,
+  });
+}
